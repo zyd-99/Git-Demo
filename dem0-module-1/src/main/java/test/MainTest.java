cfdd33c8aceaab8ebc8696ecb4cc0bf482efe781
zyd-99 <1289273264@qq.com>
2020-10-10 15:38:32 +0800

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	dem0-module-1/src/main/java/test/MainTest.java

--- dem0-module-1/src/main/java/test/MainTest.java
+++ dem0-module-1/src/main/java/test/MainTest.java
@@ -4,11 +4,7 @@ public class MainTest {
 
     public static void  main(String []args){
         System.out.println("hello git");
-<<<<<<< HEAD
-
-=======
         System.out.println("test 退回");
->>>>>>> origin/master
     }
 
 }
